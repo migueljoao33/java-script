2ff499a61886452c0b0de008b6e809385bb29e65
João Miguel <joaomiguelalbuquerque2303@gmail.com>
2025-10-03 16:12:56 -0300

    js

---
+++ ex05/ex05.js
@@ -0,0 +1,9 @@
+var vel = 61
+console.log(`a velocidade do seu carro é de ${vel}Km/h`)
+
+if (vel > 60) {
+    true
+    console.log(`você foi multado por andar à ${vel}Km/h`)
+} else {
+    console.log('pode passar')
+}
