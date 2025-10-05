3e471ff093dfa131508f3147d95a2b894ace5b00
João Miguel <joaomiguelalbuquerque2303@gmail.com>
2025-10-05 15:59:13 -0300

    js

--- ex/ex07/script.js
+++ ex/ex07/script.js
@@ -1,16 +1,3 @@
-/*
-var c = 1
-tem esse: while (c <= 5) {
-    console.log(`plus ${c}`)
-    c++
-} e esse
- ----
-do {
-    console.log(`plus ${c}`)
-    c++
-} while (c <= 5)
-tmb tem esse  
-*/
 console.log('vai começar..')
 for (var c = 1;(c <= 5);c++) {
     console.log(`plus ${c}`)
