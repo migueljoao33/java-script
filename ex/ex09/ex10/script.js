73ec82a36b03974975bc383e4b01b4ba417be8d2
João Miguel <joaomiguelalbuquerque2303@gmail.com>
2025-10-05 21:14:33 -0300

    js

---
+++ ex/ex09/ex10/script.js
@@ -0,0 +1,11 @@
+function contar() {
+    let ini = document.getElementById('c1')
+    let fim = document.getElementById('c2')
+    let passo = document.getElementById('c3')
+
+    if (c1.value.lenght == 0 || c2.value.lenght == 0 || c3.value.lenght == 0 ) {
+        alert('the data is not complete')
+    } else {
+        confirm('tudo ok')
+    }
+ }
