502cf3896b85dd2e22b1978d3c14a33d6ea089f4
João Miguel <joaomiguelalbuquerque2303@gmail.com>
2025-10-08 14:12:40 -0300

    js

--- ex/ex09/ex11/script.js
+++ ex/ex09/ex11/script.js
@@ -1,47 +1,17 @@
-function verify() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('dn')
-    var res = document.getElementById('res')
-    var img = document.getElementById('photo')
-
-    if (fano.value.lenght == 0 || Number(fano.value) > ano) {
-        alert('try again')
+function tabuada() {
+    var num = document.getElementById('n')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        alert('digite um número')
     } else {
-        var sex = document.getElementsByName('radgender')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-    } 
-    
-    if (sex[0].checked) {
-        genero = 'man'
-        if (idade >= 0 && idade < 3) {
-            img.setAttribute('src', "imgs/bebe-m.jpg")
-        } else if (idade >= 3 && idade < 12) {
-            img.setAttribute('src', "imgs/crianca-m.jpeg")
-        } else if (idade >= 12 && idade < 18) {
-            img.setAttribute('src', "imgs/adolescente-m.jpg")
-        } else if (idade >= 18 && idade < 60) {
-            img.setAttribute('src', "imgs/adulto-m.jpg")
-        } else if (idade >= 60) {
-            img.setAttribute('src', "imgs/idoso-m.jpg")
-        }
+        var n = Number(num.value)
+        var c = 1
 
-    } else if (sex[1].checked) {
-        genero = 'woman'
-        if (idade >= 0 && idade < 3) {
-            img.setAttribute('src', "imgs/bebe-f.jpg")
-        } else if (idade >= 3 && idade < 12) {
-            img.setAttribute('src', "imgs/crianca-f.jpg")
-        } else if (idade >= 12 && idade < 18) {
-            img.setAttribute('src', "imgs/adolescente-f.jpg")
-        } else if (idade >= 18 && idade < 60) {
-            img.setAttribute('src', "imgs/adulto-f.jpg")
-        } else if (idade >= 60) {
-            img.setAttribute('src', "imgs/idoso-f.jpg")
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
         }
     }
-
-    res.style.textAlign = 'center'
-    res.innerHTML = `detected ${genero} and ${idade} years old`
 }
