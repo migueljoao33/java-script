212ab32dae2a917bd85e94e6386d4f57ffea353b
João Miguel <joaomiguelalbuquerque2303@gmail.com>
2025-10-09 20:48:21 -0300

    js

---
+++ ex/ex13/script.js
@@ -0,0 +1,34 @@
+// var res = parimp(4)
+// console.log(res) 
+
+// function parimp(n) {
+//     if (n%2 == 0) {
+//         return 'par'
+//     } else {
+//         return 'ímpar'
+//     }
+// }
+
+
+// function soma(n1 = 0, n2 = 0) {
+//     return n1 + n2
+// }
+// console.log(soma(4))
+
+
+// var res = function(x) {
+//     return x*2
+// }
+// console.log(res(2))
+
+
+// function fatorial(n) {
+//     var fat = 1
+//     for (var c = n; c > 1; c--) {
+//         fat *= c
+//     }
+//     return fat
+// }
+// console.log(fatorial(5))
+
+
